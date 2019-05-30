a90d4de48904a544bad891c7213532501c8c2693
isabellevieirab <49079428+isabellevieirab@users.noreply.github.com>
2019-05-30 12:40:30 -0300

    Update produto.h

--- include/produto/produto.h
+++ include/produto/produto.h
@@ -5,25 +5,23 @@
 #include <string>
 #include <vector>
 
-class Produtos{
+class Produto{
     public:
         float _preco;
         std::vector<int> _avaliacoes;
         float _mediaAvaliacoes;
         std::vector<std::string> _comentarios;
         std::string _nome;
-        std::string _categoria; //(Tema) [string]
-        std::string _cor; // [string]
-        std::string _descricao;// [string]
-        std::string _material;// [string]
-        Produtos(float preco, float mediaAvaliacoes, std::string nome, std::string categoria, std::string cor, std::string descricao, std::string material);
-        ~Produtos();
-        std::string getProduto(); //exibe produto
-        /*std::string setProduto(float, float, std::string,
-        std::string, std::string, std::string, std::string); //cria produto*/
-        void avaliarProduto(); //salva avaliacao na lista e depois modifica a media
+        std::string _categoria; //(Tema) 
+        std::string _cor; 
+        std::string _descricao;
+        std::string _material;
+        Produto(float preco, float mediaAvaliacoes, std::string nome, std::string categoria, std::string cor, std::string descricao, std::string material);
+        ~Produto();
+        std::string getProduto(); //exibe produto 
+        void avaliarProduto(int nota); //salva avaliacao na lista e depois modifica a media
         void getComentarios(); //imprime comentarios
-        void setComentario(); //salva comentario
+        void setComentario(std::string comentario); //salva comentario
 };
 
 #endif
