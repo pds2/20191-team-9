e7bbb6d7282ebe7f2b81b4f36ceca4f74d40d892
isabellevieirab <49079428+isabellevieirab@users.noreply.github.com>
2019-06-04 22:01:18 -0300

    Update and rename camisa.h to blusas_e_moletom.h

--- include/produto/blusas_e_moletom.h
+++ include/produto/blusas_e_moletom.h
@@ -1,15 +1,15 @@
-#ifndef CAMISA_H
-#define CAMISA_H
+#ifndef BLUSAS_E_MOLETOM_H
+#define BLUSAS_E_MOLETOM_H
 
 #include <iostream>
 #include <string>
 #include <vector>
 #include "produto.h"
 
-class Camisa : public Produto{
+class BlusasEMoletom : public Produto{
     public:
         char _tamanho;
-        std::string _tipo;
+        std::string _tipo; //Se é uma blusa ou um moletom
     
         void setProduto(float preco, float mediaAvaliacoes, std::string nome, std::string categoria, std::string cor, std::string descricao, std::string material, char tamanho, std::string tipo);
         void getProduto();
