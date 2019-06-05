9c4d3a054dfcb865f0939590444ae5822a3ec278
isabellevieirab <49079428+isabellevieirab@users.noreply.github.com>
2019-06-04 21:48:25 -0300

    Update caneca.cpp

---
+++ src/produto/caneca.cpp
@@ -0,0 +1,23 @@
+#include "../../include/produto/caneca.h"
+#include "produto.cpp"
+
+void Caneca::setProduto(float preco, float mediaAvaliacoes, std::string nome, std::string categoria, std::string cor, std::string descricao, std::string material, int diametro){
+    
+        std::vector<std::string> vazio;
+        std::vector<int> nada;
+
+        _preco = preco;
+        _mediaAvaliacoes = mediaAvaliacoes;
+        _nome = nome;
+        _categoria = categoria;
+        _cor = cor;
+        _descricao = descricao;
+        _material = material;
+        _comentarios = vazio;
+        _avaliacoes = nada;
+        _diametro = diametro;    
+}
+
+void Caneca::getProduto(){
+    std::cout << "Nome:\t" <<_nome << std::endl << "Preco:\t"<< _preco << std::endl << "Media:\t" <<_mediaAvaliacoes << std::endl << "Categoria:\t" << _categoria << std::endl << "Cor:\t" <<_cor << std::endl << "Descricao:\t" << _descricao << std::endl << "Material:\t" << _material << std::endl << "Diametro:\t" << _diametro << std::endl;
+}
