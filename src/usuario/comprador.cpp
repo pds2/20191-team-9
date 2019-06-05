8818b4ff274096b2a92371cc2f012c4de3ab47b7
aikaterhine <catarina.edp@gmail.com>
2019-06-05 15:25:24 -0300

    update comprador.cpp

--- src/usuario/comprador.cpp
+++ src/usuario/comprador.cpp
@@ -1,4 +1,30 @@
 #ifndef COMPRADOR_CPP
 #define COMPRADOR_CPP
 
+Comprador(){
+
+}
+
+~Comprador(){
+
+}
+
+bool adicionarCarrinho(){
+
+}
+
+bool excluirCarrinho(){
+
+}
+
+bool adicionaDinheiro(){
+
+}
+
+
+void exibirPerfil(){
+
+}
+
+
 #endif
