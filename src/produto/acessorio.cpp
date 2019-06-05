4726464c7d04ba70fa9bd39db23d04b063a1fbcb
Victoria <45579178+victoriasaliba@users.noreply.github.com>
2019-06-05 11:15:42 -0300

    Update acessorio.cpp

--- src/produto/acessorio.cpp
+++ src/produto/acessorio.cpp
@@ -1,5 +1,5 @@
-/*
-#include "../../include/produto/acessorio.h"
+
+#include "produto/acessorio.h"
 #include "produto.cpp"
 
 void Acessorio::setProduto(float preco, float mediaAvaliacoes, std::string nome, std::string categoria, std::string cor, std::string descricao, std::string material, std::string tipo){
@@ -22,4 +22,4 @@ void Acessorio::setProduto(float preco, float mediaAvaliacoes, std::string nome,
 void Acessorio::getProduto(){
     std::cout <<"Nome:\t" <<_nome << std::endl << "Preco:\t"<< _preco << std::endl << "Media:\t" <<_mediaAvaliacoes << std::endl << "Categoria:\t" << _categoria << std::endl << "Cor:\t" <<_cor << std::endl << "Descricao:\t" << _descricao << std::endl << "Material:\t" <<_material << std::endl << "Tipo:\t" << _tipo << std::endl;
 }
-*/
+
