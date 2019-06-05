55d6d14ee41eda2df019c1cceb53e40d6c3a5182
isabellevieirab <49079428+isabellevieirab@users.noreply.github.com>
2019-06-04 22:03:39 -0300

    Update and rename camisa.cpp to blusas_e_moletom.cpp

--- src/produto/blusas_e_moletom.cpp
+++ src/produto/blusas_e_moletom.cpp
@@ -1,7 +1,7 @@
-#include "../../include/produto/camisa.h"
+#include "../../include/produto/blusas_e_moletom.h"
 #include "produto.cpp"
 
-void Camisa::setProduto(float preco, float mediaAvaliacoes, std::string nome, std::string categoria, std::string cor, std::string descricao, std::string material, char tamanho, std::string tipo){
+void BlusasEMoletom::setProduto(float preco, float mediaAvaliacoes, std::string nome, std::string categoria, std::string cor, std::string descricao, std::string material, char tamanho, std::string tipo){
     
         std::vector<std::string> vazio;
         std::vector<int> nada;
@@ -19,6 +19,6 @@ void Camisa::setProduto(float preco, float mediaAvaliacoes, std::string nome, st
         _tamanho = tamanho;    
 }
 
-void Camisa::getProduto(){
+void BlusasEMoletom::getProduto(){
     std::cout <<"Nome:\t" <<_nome << std::endl << "Preco:\t"<< _preco << std::endl << "Media:\t" <<_mediaAvaliacoes << std::endl << "Categoria:\t" << _categoria << std::endl << "Cor:\t" <<_cor << std::endl << "Descricao:\t" << _descricao << std::endl << "Material:\t" <<_material << std::endl << "Tipo:\t" << _tipo << std::endl<< "Tamanho:\t" << _tamanho << std::endl;
 }
