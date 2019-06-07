e30d4432f7e4297fdfd2be84942fb99302136486
Victoria <45579178+victoriasaliba@users.noreply.github.com>
2019-06-07 17:44:25 -0300

    Update acessorio.h

--- include/produto/acessorio.h
+++ include/produto/acessorio.h
@@ -9,7 +9,8 @@
 class Acessorio : public Produto{
     public:
         std::string _tipo;
-        
+        Acessorio();
+        ~Acessorio();
         void setProduto(float preco, float mediaAvaliacoes, std::string nome, std::string categoria, std::string cor, std::string descricao, std::string material, std::string tipo);
         void getProduto();
 };
