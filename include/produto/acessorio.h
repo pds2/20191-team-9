80bf24b1ec0ae3dc598f568acabc6dece20c4821
isabellevieirab <49079428+isabellevieirab@users.noreply.github.com>
2019-06-08 01:31:44 -0300

    Update acessorio.h

--- include/produto/acessorio.h
+++ include/produto/acessorio.h
@@ -9,9 +9,10 @@
 class Acessorio : public Produto{
     public:
         std::string _tipo;
-        Acessorio();
+       
+        Acessorio(float preco, float mediaAvaliacoes, std::string nome, std::string categoria, std::string cor, std::string descricao, std::string material, std::string tipo);
         ~Acessorio();
-        void setProduto(float preco, float mediaAvaliacoes, std::string nome, std::string categoria, std::string cor, std::string descricao, std::string material, std::string tipo);
+        //void setProduto(float preco, float mediaAvaliacoes, std::string nome, std::string categoria, std::string cor, std::string descricao, std::string material, std::string tipo);
         void getProduto();
 };
 
