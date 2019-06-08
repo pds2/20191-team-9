ea93c8b1b15a118bd5d067b6324db43b142f9b25
isabellevieirab <49079428+isabellevieirab@users.noreply.github.com>
2019-06-08 01:29:17 -0300

    Update blusas_e_moletom.cpp

--- src/produto/blusas_e_moletom.cpp
+++ src/produto/blusas_e_moletom.cpp
@@ -5,13 +5,18 @@
 #include "produto.cpp"
 
 
-BlusasEMoletom::BlusasEMoletom(){
+
+BlusasEMoletom::BlusasEMoletom(float preco, float mediaAvaliacoes, std::string nome, std::string categoria, std::string cor, std::string descricao, std::string material, char tamanho, std::string tipo) : Produto(preco, mediaAvaliacoes, nome, categoria, cor, descricao, material){
+        _tipo = tipo;
+        _tamanho = tamanho;    
 }
+
 BlusasEMoletom::~BlusasEMoletom(){
+
 }
 
-void BlusasEMoletom::setProduto(float preco, float mediaAvaliacoes, std::string nome, std::string categoria, std::string cor, std::string descricao, std::string material, char tamanho, std::string tipo){
-
+/*void BlusasEMoletom::setProduto(float preco, float mediaAvaliacoes, std::string nome, std::string categoria, std::string cor, std::string descricao, std::string material, char tamanho, std::string tipo){
+    
         std::vector<std::string> vazio;
         std::vector<int> nada;
 
@@ -25,8 +30,8 @@ void BlusasEMoletom::setProduto(float preco, float mediaAvaliacoes, std::string 
         _comentarios = vazio;
         _avaliacoes = nada;
         _tipo = tipo;
-        _tamanho = tamanho;
-}
+        _tamanho = tamanho;    
+}*/
 
 void BlusasEMoletom::getProduto(){
     std::cout <<"Nome:\t" <<_nome << std::endl << "Preco:\t"<< _preco << std::endl << "Media:\t" <<_mediaAvaliacoes << std::endl << "Categoria:\t" << _categoria << std::endl << "Cor:\t" <<_cor << std::endl << "Descricao:\t" << _descricao << std::endl << "Material:\t" <<_material << std::endl << "Tipo:\t" << _tipo << std::endl<< "Tamanho:\t" << _tamanho << std::endl;
