c4d9d38662eca6fb37f96103d4b9b91a86516089
isabellevieirab <49079428+isabellevieirab@users.noreply.github.com>
2019-06-09 17:18:06 -0300

    Update caneca.cpp

--- src/produto/caneca.cpp
+++ src/produto/caneca.cpp
@@ -4,32 +4,27 @@
 #include "produto/caneca.h"
 #include "produto.cpp"
 
-Caneca::Caneca(float preco, float mediaAvaliacoes, std::string nome, std::string categoria, std::string cor, std::string descricao, std::string material, int diametro):Produto(preco, mediaAvaliacoes, nome, categoria, cor, descricao, material){
+Caneca::Caneca(int cod, float preco, float mediaAvaliacoes, std::string nome, std::string categoria, std::string cor, std::string descricao, std::string material, float diametro):Produto(cod, preco, mediaAvaliacoes, nome, categoria, cor, descricao, material){
 
-        _diametro = diametro;
+        this->_diametro = diametro;
 }
 
 Caneca::~Caneca(){
 }
 
-void Caneca::setProduto(float preco, float mediaAvaliacoes, std::string nome, std::string categoria, std::string cor, std::string descricao, std::string material, int diametro){
-
-        std::vector<std::string> vazio;
-        std::vector<int> nada;
-
-        _preco = preco;
-        _mediaAvaliacoes = mediaAvaliacoes;
-        _nome = nome;
-        _categoria = categoria;
-        _cor = cor;
-        _descricao = descricao;
-        _material = material;
-        _comentarios = vazio;
-        _avaliacoes = nada;
-        _diametro = diametro;
+float Caneca::getDiametro(){
+        return this->_diametro;
 }
 
-void Caneca::getDiametro(){
-    std::cout<<"Diametro:/t"<<_diametro;
-
+void Caneca::imprimeProduto(){
+        std::cout << "------------------------------" << std::endl;
+        std::cout << "Nome do produto:\t" << getNome() << std::endl;
+        std::cout << "Preco:\t\t\tR$ "<< getPreco() << std::endl;
+        std::cout << "Cor:\t\t\t" << getCor() << std::endl;
+        std::cout << "Media de avaliacoes:\t" << getMediaAvaliacoes() << std::endl;
+        std::cout << "Categoria:\t\t" << getCategoria() << std::endl;
+        std::cout << "Descricao:\t\t" << getDescricao() << std::endl;
+        std::cout << "Material:\t\t" << getMaterial() << std::endl;
+        std::cout << "Diametro:\t\t" << getDiametro() << " cm" << std::endl;
+}
 #endif
