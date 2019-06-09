8d2f62551e88f8b860565f2fa186b1f2617821e3
Victoria <45579178+victoriasaliba@users.noreply.github.com>
2019-06-09 12:14:35 -0300

    Update caneca.cpp
    get separado

--- src/produto/caneca.cpp
+++ src/produto/caneca.cpp
@@ -1,4 +1,4 @@
-/*#ifndef CANECA_CPP
+#ifndef CANECA_CPP
 #define CANECA_CPP
 
 #include "produto/caneca.h"
@@ -10,9 +10,9 @@ Caneca::Caneca(float preco, float mediaAvaliacoes, std::string nome, std::string
 }
 
 Caneca::~Caneca(){
-}*/
+}
 
-/*void Caneca::setProduto(float preco, float mediaAvaliacoes, std::string nome, std::string categoria, std::string cor, std::string descricao, std::string material, int diametro){
+void Caneca::setProduto(float preco, float mediaAvaliacoes, std::string nome, std::string categoria, std::string cor, std::string descricao, std::string material, int diametro){
 
         std::vector<std::string> vazio;
         std::vector<int> nada;
@@ -27,10 +27,9 @@ Caneca::~Caneca(){
         _comentarios = vazio;
         _avaliacoes = nada;
         _diametro = diametro;
-}*/
-
-/*void Caneca::getProduto(){
-    std::cout << "Nome:\t" <<_nome << std::endl << "Preco:\t"<< _preco << std::endl << "Media:\t" <<_mediaAvaliacoes << std::endl << "Categoria:\t" << _categoria << std::endl << "Cor:\t" <<_cor << std::endl << "Descricao:\t" << _descricao << std::endl << "Material:\t" << _material << std::endl << "Diametro:\t" << _diametro << std::endl;
 }
 
-#endif*/
+void Caneca::getDiametro(){
+    std::cout<<"Diametro:/t"<<_diametro;
+
+#endif
