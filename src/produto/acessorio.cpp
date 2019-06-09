a9f3efcd1c701c7926b13459f81c9fc83d36e9e6
isabellevieirab <49079428+isabellevieirab@users.noreply.github.com>
2019-06-09 16:39:27 -0300

    Update acessorio.cpp

--- src/produto/acessorio.cpp
+++ src/produto/acessorio.cpp
@@ -4,31 +4,30 @@
 #include "produto/acessorio.h"
 #include "produto.cpp"
 
-Acessorio::Acessorio(float preco, float mediaAvaliacoes, std::string nome, std::string categoria, std::string cor, std::string descricao, std::string material, std::string tipo):Produto(preco, mediaAvaliacoes, nome, categoria, cor, descricao, material){
-    _tipo = tipo;
+Acessorio::Acessorio(int cod, float preco, float mediaAvaliacoes, std::string nome, std::string categoria, std::string cor, std::string descricao, std::string material, std::string tipo):Produto(cod, preco, mediaAvaliacoes, nome, categoria, cor, descricao, material){
+    this->_tipo = tipo;
 }
 
 Acessorio::~Acessorio(){
 
 }
 
-void Acessorio::setProduto(float preco, float mediaAvaliacoes, std::string nome, std::string categoria, std::string cor, std::string descricao, std::string material, std::string tipo){
-
-        std::vector<std::string> vazio;
-        std::vector<int> nada;
-
-        _preco = preco;
-        _mediaAvaliacoes = mediaAvaliacoes;
-        _nome = nome;
-        _categoria = categoria;
-        _cor = cor;
-        _descricao = descricao;
-        _material = material;
-        _comentarios = vazio;
-        _avaliacoes = nada;
-        _tipo = tipo;
+std::string Acessorio::getTipo(){
+    return this->_tipo;
 }
-void Acessorio::getTipo(){
-    std::cout<<"Tipo:/t"<<_tipo;
+
+void Acessorio::imprimeProduto(){
+    
+    std::cout << "------------------------------" << std::endl;
+    std::cout << "Nome do produto:\t" << getNome() << std::endl;
+    std::cout << "Preco:\t\t\tR$ "<< getPreco() << std::endl;
+    std::cout << "Cor:\t\t\t" << getCor() << std::endl;
+    std::cout << "Media de avaliacoes:\t" << getMediaAvaliacoes() << std::endl;
+    std::cout << "Categoria:\t\t" << getCategoria() << std::endl;
+    std::cout << "Descricao:\t\t" << getDescricao() << std::endl;
+    std::cout << "Material:\t\t" << getMaterial() << std::endl;
+    std::cout << "Tipo:\t\t\t" << getTipo() << std::endl;
+
+}
 
 #endif
