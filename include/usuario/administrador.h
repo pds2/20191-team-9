3ee856f9ed5b9a6e61092cc1fcd6a8a308223a9e
LarissaGS <31114488+LarissaGS@users.noreply.github.com>
2019-06-09 21:59:34 -0300

    Update administrador.h
    Primeira versão do .h

---
+++ include/usuario/administrador.h
@@ -0,0 +1,41 @@
+#ifndef ADMINISTRADOR_H
+#define ADMINISTRADOR_H
+
+#include <string>
+#include <iostream>
+
+#include "ecommerce.h"
+#include "produto/produto.h"
+#include "usuario/usuario.h"
+#include "usuario/comprador.h"
+
+class Administrador : public Usuario{
+  private:
+    std::str _requisicoes[];
+
+  public:
+    Administrador(std::string nome, std::string email, std::string senha);
+
+    void removeItem(Produto item);
+
+    void adicionaItem(std::string nome_do_produto);
+
+    void mostraPedidos();
+
+    bool aprovaPedido(std::string requisicao, Comprador user);
+
+    void exibeUsuarios();
+
+    void editaUsuario(Comprador user);
+
+    void excluiUsuario(Comprador user);
+
+    void exibeMensagens(Comprador user);
+
+    void respondeMensagens(Comprador user);
+
+    ~Administrador();
+
+};
+
+#endif
