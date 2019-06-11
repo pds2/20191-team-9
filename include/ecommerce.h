d4365cdb146623764a4592db30340f79fca22716
Tati Álvares <tatiana.oliveira@ijunior.com.br>
2019-06-11 18:24:53 -0300

    Update ecommerce.h
    Adição de funções

--- include/ecommerce.h
+++ include/ecommerce.h
@@ -18,12 +18,8 @@ class Ecommerce{
   private:
     std::vector <Usuario> usuarios;
     std::vector <Comprador> compradores;
-
-  /*std::vector <Produto> produtos;
-    std::vector <Caneca> canecas;
-    std::vector <Acessorio> acessorios;
-    std::vector <BlusasEMoletom> blusasEmoletons;*/
-
+    const std::string SENHADMIN = "Ecom@Geek42"
+    Ecommerce::Usuario *userLogged = NULL;
 
   public:
     Ecommerce();
@@ -31,35 +27,36 @@ class Ecommerce{
 
     void cadastrarUsuario (std::string n, std::string em, std::string s);
     void cadastrarComprador (std::string n, std::string em, std::string s, std::string cpf, std::string endereco, int numCarr, int numHist, int numAval, double din);
-    //void cadastrarCaneca(int cod, float preco, float mediaAvaliacoes, std::string nome, std::string cor, std::string descricao, std::string material, float diametro);
-    //void cadastrarAcessorio(int cod, float preco, float mediaAvaliacoes, std::string nome, std::string cor, std::string descricao, std::string material, std::string tipo);
-    //void cadastrarBlusasEMoletom(int cod, float preco, float mediaAvaliacoes, std::string nome, std::string cor, std::string descricao, std::string material, char tamanho, std::string tipo);
-
     bool procurarUsuario(std::string email);
     bool procurarComprador(std::string em);
     void imprimirUsuarios();
     void imprimirCompradores();
-    void imprimirProdutos();
 
-    bool checaCodigo(int cod); //verifica se já existe um produto com o mesmo codigo cadastrado, caso sim retorna false
+    //void imprimirProdutos();
+    //void imprimirMensagens();
+
     bool checaNome(std::string n);
     bool checaEmail(std::string em);
     bool checaSenha(std::string s);
-
-    /*int buscaIndiceCaneca(int cod);
-    int buscaIndiceAcessorio(int cod);
-    int buscaIndiceBlusasEMoletom(int cod);*/
+    bool checaSenhaAdmin(std::string sAdmin);
+    //Validar e form cadastro usuario e cadastrarUsuario
+    //Falso volta pra ecommerce()
 
     void addCarrinho();
     void limparTela();
-
     void impCarrinho();
     void impHistorico();
     void listaUsuarioArquivo();
     void gravaUsuarioArquivo();
-    //void listaProdutosArquivo();    //ERRO
-    //void gravaProdutosArquivo();
 
+    void loginUsuario(std::string n, std::string s);
+    void logoutUsuario();
+    void inicio();
+    void menuSumario(int idMenu, int opcao);
+    //opção voltar
+    //opção logout geral
+    void menuUsuario();
+    void menuComprador();
 };
 
 #endif
