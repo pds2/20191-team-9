173969434316725a340e2f9b7ca4800c98e7ca9f
LarissaGS <31114488+LarissaGS@users.noreply.github.com>
2019-06-11 17:22:35 -0300

    Update editaUsuario e excluiUsuario
    As funções agora recebem o email do usuário em questão como parâmetro.

--- include/usuario/administrador.h
+++ include/usuario/administrador.h
@@ -28,13 +28,13 @@ class Administrador : public Usuario{
 
     void exibeUsuarios();
 
-    void editaUsuario(Comprador user);
+    void editaUsuario(std::string email);
 
-    void excluiUsuario(Comprador user);
+    void excluiUsuario(std::string email);
 
-    void exibeMensagens(Comprador user);
+    void exibeMensagens(std::string email);
 
-    void respondeMensagens(Comprador user);
+    void respondeMensagens(std::string email);
 
     ~Administrador();
 
