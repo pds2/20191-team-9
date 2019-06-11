b55ee4ac4cdb0e496671f10f83773976d6652500
isabellevieirab <49079428+isabellevieirab@users.noreply.github.com>
2019-06-10 23:23:45 -0300

    Update ecommerce.h

--- include/ecommerce.h
+++ include/ecommerce.h
@@ -18,6 +18,12 @@ class Ecommerce{
   private:
     std::vector <Usuario> usuarios;
     std::vector <Comprador> compradores;
+  
+    std::vector <Produto> produtos;
+    std::vector <Caneca> canecas;
+    std::vector <Acessorio> acessorios;
+    std::vector <BlusasEMoletom> blusasEmoletons;
+
 
   public:
     Ecommerce();
@@ -25,11 +31,17 @@ class Ecommerce{
 
     void cadastrarUsuario (std::string n, std::string em, std::string s);
     void cadastrarComprador (std::string n, std::string em, std::string s, std::string cpf, std::string endereco, int numCarr, int numHist, int numAval, double din);
+    void cadastrarCaneca(int cod, float preco, float mediaAvaliacoes, std::string nome, std::string cor, std::string descricao, std::string material, float diametro);
+    void cadastrarAcessorio(int cod, float preco, float mediaAvaliacoes, std::string nome, std::string cor, std::string descricao, std::string material, std::string tipo);
+    void cadastrarBlusasEMoletom(int cod, float preco, float mediaAvaliacoes, std::string nome, std::string cor, std::string descricao, std::string material, char tamanho, std::string tipo);
+  
     bool procurarUsuario(std::string email);
     bool procurarComprador(std::string em);
     void imprimirUsuarios();
     void imprimirCompradores();
-
+    void imprimirProdutos();
+    
+    bool checaCodigo(int cod); //verifica se já existe um produto com o mesmo codigo cadastrado, caso sim retorna false
     bool checaNome(std::string n);
     bool checaEmail(std::string em);
     bool checaSenha(std::string s);
@@ -41,6 +53,9 @@ class Ecommerce{
     void impHistorico();
     void listaUsuarioArquivo();
     void gravaUsuarioArquivo();
+    void listaProdutosArquivo();    //ERRO
+    void gravaProdutosArquivo();
+  
 };
 
 #endif
