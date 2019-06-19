f3ab4a40fc1a66a16aa539aaccd3f730cc11aadb
Victoria <45579178+victoriasaliba@users.noreply.github.com>
2019-06-18 21:24:45 -0300

    Update tester.cpp

---
+++ program/tester.cpp
@@ -0,0 +1,24 @@
+include "doctest.h"
+#include <iostream>
+#include "ecommerce.h"
+
+
+TEST_CASE("Declaracão produto") {
+    CHECK_NOTHROW(BlusasEMoletom blusa(2, 60, 4.5, "Moletom amarelo", "Moletom", "Amarelo", "Amarelo que nem o raiar do sol", "Algodao", M, "Tipo tiposo"));
+    CHECK_NOTHROW(BlusasEMoletom blusa ());
+}
+
+TEST_CASE("Get produto") {
+    BlusasEMoletom m(2, 60, 4.5, "Moletom amarelo", "Moletom", "Amarelo", "Amarelo que nem o raiar do sol", "Algodao", M, "Tipo tiposo");
+    CHECK_EQ(m.getCodigoProduto(),2);
+    CHECK_EQ(m.getPreco(),60);
+    CHECK_EQ(m.getMediaAvaliacoes(),4.5);
+    CHECK_EQ(m.getNome(),"Moletom Amarelo");
+    CHECK_EQ(m.getCategoria(),"Moletom");
+    CHECK_EQ(m.getCor(),"Amarelo");
+    CHECK_EQ(m.getDescricao(),"Amarelo que nem o raiar do sol");
+    CHECK_EQ(m.getMaterial(),"Algodao");
+    CHECK_EQ(m.getTamanho(), M);
+    CHECK_EQ(m.getTipo(),"Tipo tiposo");
+
+}
