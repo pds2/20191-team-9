5de589b2306bb4d96a0745655021404999022db9
aikaterhine <catarina.edp@gmail.com>
2019-06-20 18:58:05 -0300

    updates testes funcionando, menos o de adm

--- program/tester.cpp
+++ program/tester.cpp
@@ -6,6 +6,8 @@
 #include "produto/caneca.h"
 #include "produto/acessorio.h"
 #include "usuario/comprador.h"
+#include "usuario/administrador.h"
+#include "ecommerce.h"
 
 TEST_CASE("Declaracão produto") {
     CHECK_NOTHROW(Produto p(2, 60, 5, "Produto Diferenciavel", "Teste", "Verde", "Produto otimo para ser utilizado em casos de teste", "Radiacao"));
@@ -41,7 +43,6 @@ TEST_CASE("Get produto - Caneca") {
     CHECK_EQ(c.getDiametro(), 5.5);
 }
 
-
 TEST_CASE("Get produto - Acessorios") {
     Acessorio a(4, 20, 4, "Pulseira GOT", "Pulseira", "Vermelha", "Pulseira Dracarys", "Couro", "Tiposo");
     CHECK_EQ(a.getCodigoProduto(),4);
@@ -55,54 +56,18 @@ TEST_CASE("Get produto - Acessorios") {
     CHECK_EQ(a.getTipo(),"Tiposo");
 }
 
-/*
-//-----> Testes ADMINISTRADOR
-TEST_CASE("Criar o Administrador"){
-  Administrador admin();
-  CHECK_EQ(admin.getNome(), "ADMIN");
-  CHECK_EQ(admin.getEmail(), "admin1@gmail.com");
-  CHECK_EQ(admin.getSenha(), "123tasalvo");
-}
-//implementar pós exceções
-TEST_CASE("Remover item do estoque"){
-  BlusasEMoletom teste(697, 45, 3.5, "blusazul", "Blusa e Moletom", "Amarelo", "Amarelo que nem o raiar do sol", "Algodao", M, "Tipo tiposo");
-}
-
-TEST_CASE("Remover item que não existe"){
-  CHECK_EQ(removeItem(naoexiste), -1);
-}
-
-TEST_CASE("Aumentar saldo de usuário que não existe"){
-  CHECK_EQ(aprovaPedido(naoexiste), -1);
-}
-
-TEST_CASE("Aumentar saldo de usuário que não requisitou o aumento"){
-  Comprador shops("semaumento","sem@aumento.com","senha123","44565564","nãoaumenta",6,6,4,5);
-  CHECK_EQ(aprovaPedido(sem@aumento.com), -2);
-}
-//implementar pós exceções
-TEST_CASE("Aprovar um pedido"){
-
-}
-
-TEST_CASE("Excluir usuário que não existe"){
-  CHECK_EQ(excluiUsuario(naoexiste), -1);
-}
-//implementar pós exceções
-TEST_CASE("Excluir um usuário"){
-  Comprador bye("excluir","ex@cluir.com","excluido","000000","exclui",7,6,3,9);
-}
-*/
 //-----> Testes COMPRADOR
 
 TEST_CASE("Cadastrar Comprador") {
-    CHECK_NOTHROW(Comprador comp ("Arthur Moura", "arthur.moura@gmail.com", "1234", "123456789", "Rua Washington, 80. Copacabana.", 0, 0, 0, 0.0));
-    CHECK_NOTHROW(Comprador comp ("Carolina Enya", "carolina.enya@gmail.com", "1234", "222222222", "Rua Washington, 90. Copacabana.", 0, 0, 0, 0.0));
-    CHECK_NOTHROW(Comprador comp ("Rita Caldas", "rita.caldas@gmail.com", "1234", "33333333333", "Rua Washington, 100. Copacabana.", 0, 0, 0, 0.0));
-    CHECK_NOTHROW(Comprador comp ("Tula Valentina", "tula.valentina@gmail.com", "1234", "444444444", "Rua Washington, 110. Copacabana.", 0, 0, 0, 0.0));
+    Ecommerce ecom;
+    CHECK_NOTHROW(ecom.cadastrarComprador("Arthur Moura", "arthur.moura@gmail.com", "1234", "123456789", "Rua Washington, 80. Copacabana.", 0, 0, 0, 0.0));
+    CHECK_NOTHROW(ecom.cadastrarComprador("Carolina Enya", "carolina.enya@gmail.com", "1234", "222222222", "Rua Washington, 90. Copacabana.", 0, 0, 0, 0.0));
+    CHECK_NOTHROW(ecom.cadastrarComprador("Rita Caldas", "rita.caldas@gmail.com", "1234", "33333333333", "Rua Washington, 100. Copacabana.", 0, 0, 0, 0.0));
+    CHECK_NOTHROW(ecom.cadastrarComprador("Tula Valentina", "tula.valentina@gmail.com", "1234", "444444444", "Rua Washington, 110. Copacabana.", 0, 0, 0, 0.0));
 }
+
 TEST_CASE("Get - Dados do Comprador") {
-    Comprador comp ("Catarina Enya", "catarina.edp@gmail.com", "1234", "555555555", "Rua Washington, 120. Copacabana.", 0, 0, 0, 0.0));
+    Comprador comp ("Catarina Enya", "catarina.edp@gmail.com", "1234", "555555555", "Rua Washington, 120. Copacabana.", 0, 0, 0, 0.0);
     CHECK_EQ(comp.getNome(), "Catarina Enya");
     CHECK_EQ(comp.getEmail(), "catarina.edp@gmail.com");
     CHECK_EQ(comp.getSenha(), "1234");
@@ -113,44 +78,46 @@ TEST_CASE("Get - Dados do Comprador") {
     CHECK_EQ(comp.getDinheiro(), 0.0);
 }
 
-TEST_CASE("Adicionar Produto ao Carrinho do Comprador") {
-  Comprador comp ("Arthur Moura", "arthur.moura@gmail.com", "1234", "123456789", "Rua Washington, 80. Copacabana.", 0, 0, 0, 0.0);
-  BlusasEMoletom m(2, 60, 4.5, "Moletom Amarelo", "Moletom", "Amarelo", "Amarelo que nem o raiar do sol", "Algodao", 'M', "Tipo tiposo");
-  Caneca c(3, 15, 4.5, "Caneca formosa", "Caneca", "Branca", "Caneca fofa de Star Wars", "Porcelana", 5.5);
-  Acessorio a(4, 20, 4, "Pulseira GOT", "Pulseira", "Vermelha", "Pulseira Dracarys", "Couro", "Tiposo");
-  comp.adicionarCarrinho();
+//-----> Testes ADMINISTRADOR
+TEST_CASE("Criar o Administrador"){
+  Administrador admin;
+  CHECK_EQ(admin.getNome(), "ADMIN");
+  CHECK_EQ(admin.getEmail(), "admin1@gmail.com");
+  CHECK_EQ(admin.getSenha(), "123tasalvo");
+}
+//implementar pós exceções
+TEST_CASE("Remover item do estoque"){
+  BlusasEMoletom teste(697, 45, 3.5, "blusazul", "Blusa e Moletom", "Amarelo", "Amarelo que nem o raiar do sol", "Algodao", 'M', "Tipo tiposo");
 }
 
-TEST_CASE("Realizar Busca por Produto no Carrinho do Comprador") {
-  Comprador comp ("Carolina Enya", "carolina.enya@gmail.com", "1234", "222222222", "Rua Washington, 90. Copacabana.", 0, 0, 0, 0.0)
-  BlusasEMoletom m(2, 60, 4.5, "Moletom Amarelo", "Moletom", "Amarelo", "Amarelo que nem o raiar do sol", "Algodao", 'M', "Tipo tiposo");
-  Caneca c(3, 15, 4.5, "Caneca formosa", "Caneca", "Branca", "Caneca fofa de Star Wars", "Porcelana", 5.5);
-  Acessorio a(4, 20, 4, "Pulseira GOT", "Pulseira", "Vermelha", "Pulseira Dracarys", "Couro", "Tiposo");
-  comp.buscaCarrinho();
+TEST_CASE("Remover item que não existe"){
+  Administrador admin;
+
+  CHECK_EQ(admin.removeItem("naoexiste"), -1);
 }
 
-TEST_CASE("Realizar Busca por Produto no Histórico do Comprador") {
-  Comprador comp ("Rita Caldas", "rita.caldas@gmail.com", "1234", "33333333333", "Rua Washington, 100. Copacabana.", 0, 0, 0, 0.0)
-  BlusasEMoletom m(2, 60, 4.5, "Moletom Amarelo", "Moletom", "Amarelo", "Amarelo que nem o raiar do sol", "Algodao", 'M', "Tipo tiposo");
-  Caneca c(3, 15, 4.5, "Caneca formosa", "Caneca", "Branca", "Caneca fofa de Star Wars", "Porcelana", 5.5);
-  Acessorio a(4, 20, 4, "Pulseira GOT", "Pulseira", "Vermelha", "Pulseira Dracarys", "Couro", "Tiposo");
-  comp.buscaHistorico();
+TEST_CASE("Aumentar saldo de usuário que não existe"){
+  Administrador admin;
+
+  CHECK_EQ(admin.aprovaPedido("naoexiste"), -1);
 }
 
-/*
-bool adicionarCarrinho();
-void fazerCompras();
-bool retirarCarrinho();
-bool adicionaDinheiro();
+TEST_CASE("Aumentar saldo de usuário que não requisitou o aumento"){
+  Administrador admin;
+  Comprador shops("semaumento","sem@aumento.com","senha123","44565564","nãoaumenta",6,6,4,5);
+  CHECK_EQ(admin.aprovaPedido("sem@aumento.com"), -2);
+}
+//implementar pós exceções
+TEST_CASE("Aprovar um pedido"){
 
-void exibirPerfil();
-void imprimirCarrinho();
-void imprimirHistorico();
+}
 
-int procurarItensHistorico(int codProduto);
-int procurarItensCarrinho(int codProduto);
-void adicionarComentario();
-void avaliarItem();
+TEST_CASE("Excluir usuário que não existe"){
+  Administrador admin;
 
-Produto buscaHistorico(int codProduto);
-Produto buscaCarrinho(int codProduto);*/
+  CHECK_EQ(admin.excluiUsuario("naoexiste"), -1);
+}
+//implementar pós exceções
+TEST_CASE("Excluir um usuário"){
+  Comprador bye("excluir","ex@cluir.com","excluido","000000","exclui",7,6,3,9);
+}
